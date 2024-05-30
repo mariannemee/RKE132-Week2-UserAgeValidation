a6decb3be178e2d8f8b361606ea49ff3b9887ecb
Mari-Anne Mee <mari-anne@MariAnnesLaptop.lan>
2024-05-30 13:21:53 +0300

    create new app

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib kasutajal valida tema sugu (m/f)
+//rakendus küsib kasutajal sisestada tema perekonnanime
+//lähtudes kasutaja valikust, rakendus tervitab kasutajat järgmiselt:
+//"Welcome, Mr. [kasutaja perekonnanimi] / "Welcome, Ms. [kasutaja perekonnanimi]
+
+Console.WriteLine("Please, select your gender (m/f):");
+
+char userGender = Char.Parse(Console.ReadLine()); //loeb konsoolist maha andmeid string (Sõne) formaadis
+
+Console.WriteLine("Please, enter your lastname:");
+string userLastName = Console.ReadLine();
+
+if(userGender == 'm')
+
+{
+    Console.WriteLine($"Welcome, Mr. {userLastName!");
+}
+else if(userGender == 'f')
+{
+    Console.WriteLine("$Welcome, Ms. {userLastName!");
+}
+Console.WriteLine($"Welcome, {userGender});
+
